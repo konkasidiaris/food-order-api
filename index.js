c1d8c69aea764f53f55ed38152bc39d9044ac9d0
Konstantinos Kasidiaris <kkasidiaris@gmail.com>
2020-05-26 17:08:29 +0300

    added nodemon and error handlind

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.use((req, res, next) => res.status(404).send("Sorry can't find that!"));
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
